25f7af5981e3d9fb55da933ae9305e09d6f30cc7
Paris Liakos <rootatwc@gmail.com>
2016-11-27 16:12:55 +0200

    Chage length of customer email so that mysql doesnt complain when creating the index

--- src/Siwapp/CustomerBundle/Entity/Customer.php
+++ src/Siwapp/CustomerBundle/Entity/Customer.php
@@ -44,7 +44,7 @@ class Customer implements \JsonSerializable
     /**
      * @var string
      *
-     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     * @ORM\Column(name="email", type="string", length=191, unique=true)
      * @Assert\Email()
      */
     private $email;
